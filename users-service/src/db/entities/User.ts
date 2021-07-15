27bd05723715df62dd4e2979771b134e1733cb31
zainzafar90 <zain@polly.ai>
2021-07-16 00:47:04 +0500

    📦 NEW: user-service database connection / entities configuration

---
+++ users-service/src/db/entities/User.ts
@@ -0,0 +1,21 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+
+@Entity("users")
+export default class User {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column()
+  username: string;
+
+  @Column({ select: false })
+  passwordHash: string;
+
+  @CreateDateColumn()
+  createdAt: string;
+}
